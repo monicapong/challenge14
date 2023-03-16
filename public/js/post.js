70f7f4016173af8febc605e64183d480750bb23d
monicapong <111400935+monicapong@users.noreply.github.com>
2023-03-16 11:39:44 -0400

    added handlebar files

---
+++ public/js/post.js
@@ -0,0 +1,21 @@
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+    console.log(id);
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+document
+  .querySelector('#new-form-post')
+  .addEventListener('click', delButtonHandler);
+
